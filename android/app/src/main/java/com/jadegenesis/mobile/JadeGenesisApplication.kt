98d403557c7f18dd0098565af96eeff93bb7c443
Vadorus <51049891+vadorus@users.noreply.github.com>
2026-09-09 14:38:20 +0200

    feat(state): start Shared Genesis State synchronization

--- android/app/src/main/java/com/jadegenesis/mobile/JadeGenesisApplication.kt
+++ android/app/src/main/java/com/jadegenesis/mobile/JadeGenesisApplication.kt
@@ -2,10 +2,12 @@ package com.jadegenesis.mobile
 
 import android.app.Application
 import com.jadegenesis.mobile.config.JadeConfigRuntime
+import com.jadegenesis.mobile.state.SharedGenesisStateWorker
 
 class JadeGenesisApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         JadeConfigRuntime.initialize(this)
+        SharedGenesisStateWorker.schedule(this)
     }
 }
